b1f2435748ebb0e10c9df39aa54161911d87aed3
in-ch <sxin2949@naver.com>
2023-06-05 20:54:34 +0900

    chore: update example

--- example/example_nextjs_app_dir/src/app/page.tsx
+++ example/example_nextjs_app_dir/src/app/page.tsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <Link href="/react-tui-date-picker">
-        <p>react-tui-date-picker</p>
+      <Link href="/next-tui-date-picker">
+        <p>next-tui-date-picker</p>
       </Link>
-      <Link href="/react-tui-range-picker">
-        <p>react-tui-range-picker</p>
+      <Link href="/next-tui-range-picker">
+        <p>next-tui-range-picker</p>
       </Link>
-      <Link href="/react-date-inline-picker">
-        <p>react-date-inline-picker</p>
+      <Link href="/next-date-inline-picker">
+        <p>next-date-inline-picker</p>
       </Link>
-      <Link href="/react-date-month-picker">
-        <p>react-date-month-picker</p>
+      <Link href="/next-date-month-picker">
+        <p>next-date-month-picker</p>
       </Link>
-      <Link href="/react-date-time-picker">
-        <p>react-date-time-picker</p>
+      <Link href="/next-date-time-picker">
+        <p>next-date-time-picker</p>
       </Link>
-      <Link href="/react-date-time-tab-picker">
-        <p>react-date-time-tab-picker</p>
+      <Link href="/next-date-time-tab-picker">
+        <p>next-date-time-tab-picker</p>
       </Link>
-      <Link href="/react-date-year-picker">
-        <p>react-date-year-picker</p>
+      <Link href="/next-date-year-picker">
+        <p>next-date-year-picker</p>
       </Link>
     </>
   );
