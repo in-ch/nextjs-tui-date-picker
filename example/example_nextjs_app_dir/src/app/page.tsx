bb21d7e2584e34ec43c82a3f2a370017ed150128
in-ch <sxin2949@naver.com>
2023-06-05 21:10:31 +0900

    fix: example linking error

--- example/example_nextjs_app_dir/src/app/page.tsx
+++ example/example_nextjs_app_dir/src/app/page.tsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <Link href="/next-tui-date-picker">
-        <p>next-tui-date-picker</p>
+      <Link href="/nextjs-tui-date-picker">
+        <p>nextjs-tui-date-picker</p>
       </Link>
-      <Link href="/next-tui-range-picker">
-        <p>next-tui-range-picker</p>
+      <Link href="/nextjs-tui-range-picker">
+        <p>nextjs-tui-range-picker</p>
       </Link>
-      <Link href="/next-date-inline-picker">
-        <p>next-date-inline-picker</p>
+      <Link href="/nextjs-date-inline-picker">
+        <p>nextjs-date-inline-picker</p>
       </Link>
-      <Link href="/next-date-month-picker">
-        <p>next-date-month-picker</p>
+      <Link href="/nextjs-date-month-picker">
+        <p>nextjs-date-month-picker</p>
       </Link>
-      <Link href="/next-date-time-picker">
-        <p>next-date-time-picker</p>
+      <Link href="/nextjs-date-time-picker">
+        <p>nextjs-date-time-picker</p>
       </Link>
-      <Link href="/next-date-time-tab-picker">
-        <p>next-date-time-tab-picker</p>
+      <Link href="/nextjs-date-time-tab-picker">
+        <p>nextjs-date-time-tab-picker</p>
       </Link>
-      <Link href="/next-date-year-picker">
-        <p>next-date-year-picker</p>
+      <Link href="/nextjs-date-year-picker">
+        <p>nextjs-date-year-picker</p>
       </Link>
     </>
   );
